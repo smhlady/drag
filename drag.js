38e638dadcf0fbad246cd7194ac31a4eed7e921b
smhlady <srlady@126.com>
2015-06-29 21:23:49 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -5,7 +5,10 @@ var disX=0;
     obj.onmousedown=function  (ev){
 ¡¡¡¡disX=ev.pageX-obj.offsetLeft;
         dis£Ù=ev.page£Ù-obj.offsetTop;
-
+document.onmousemove=function(){
+    obj.style.left=ev.clientX-disX+'px';
+    obj.style.top=ev.clientY-disY+'px';
+};
 
 
     };
