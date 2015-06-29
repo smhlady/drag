c071b49fe5c5e82810647d40299242bd9f44f66d
smhlady <srlady@126.com>
2015-06-29 22:17:07 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,17 +1,21 @@
 function drag(id){
-var obj=document.getElementById(id);
+
+    var obj=document.getElementById(id);
 var disX=0;
     var disY=0;
-    obj.onmousedown=function  (ev){
-¡¡¡¡disX=ev.pageX-obj.offsetLeft;
-        dis£Ù=ev.page£Ù-obj.offsetTop;
-document.onmousemove=function(){
- 
-    obj.style.left=ev.pageX-disX+'px';
-    obj.style.top=ev.pageY-disY+'px';
+    obj.onmousedown=function  (ev) {
+        disX = ev.pageX - obj.offsetLeft;
+        disY = ev.pageY - obj.offsetTop;
+        document.onmousemove = function (ev) {
+            obj.style.left = ev.pageX - disX + 'px';
+            obj.style.top = ev.pageY - disY + 'px';
 
-};
+        };
+        document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null;
 
-
+        };
     };
+    return false;
 }
